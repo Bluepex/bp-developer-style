ec7aab29224d718f3bd90e1ef0e15a33d38efab6
Wesley Peres <wesley.fperes@hotmail.com>
2022-10-07 16:52:01 -0300

    Change update ports builder script

--- guide_upgrade_builder_ports.sh
+++ guide_upgrade_builder_ports.sh
@@ -1,24 +1,43 @@
 #!/bin/sh
 
+rm -rf /usr/src
+rm -rf /usr/src/.*
+
 bectl list
 bectl create NOME
-bectl mount NOME (copia o ponto de montagem da saída)
+bectl mount NOME
+
 mount -t devfs devfs PONTO_DE_MONTAGEM/dev
 mount -t nullfs /usr/src PONTO_DE_MONTAGEM/usr/src
+
 chroot PONTO_DE_MONTAGEM
+
 cd /usr/src
+
+yes | pkg install git
+git clone -b main --single-branch --depth 1 https://git.FreeBSD.org/src.git /usr/src
 git pull
+
 make -s -jN buildworld buildkernel
 make -s -jN installkernel
+
 etcupdate -p
+
 make -s -jN installworld
+
 etcupdate
+
 yes | make delete-old delete-old-libs
+
 pkg upgrade
+
 umount PONTO_DE_MONTAGEM/dev
 umount PONTO_DE_MONTAGEM/usr/src
+
 bectl umount NOME
 bectl activate -t NOME
+
 shutdown -r now
+
 Se tudo ocorrer normalmente
 bectl activate NOME
